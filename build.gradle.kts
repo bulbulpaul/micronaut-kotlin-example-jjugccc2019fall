a170a2d03a9c6496b0b089aed02f2ef8a44cd3a4
Nobuo Okada <bulbulpaul0325@gmail.com>
2019-11-05 08:52:22 +0900

    convert to kts

---
+++ build.gradle.kts
@@ -0,0 +1,84 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.50"
+    kotlin("kapt") version "1.3.50"
+    kotlin("plugin.allopen") version "1.3.50"
+    id("com.github.johnrengelman.shadow") version "5.0.0"
+    id("org.jlleitschuh.gradle.ktlint") version "8.0.0"
+    id("application")
+}
+
+version = "0.1"
+group = "com.merrylab.example"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+    mavenCentral()
+    maven("https://jcenter.bintray.com")
+}
+
+val developmentOnly by configurations.creating
+configurations {
+    runtimeClasspath {
+        extendsFrom(developmentOnly)
+    }
+    testRuntimeClasspath {
+        extendsFrom(developmentOnly)
+    }
+}
+
+dependencies {
+    val kotlinVersion: String by project
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+
+    val micronautVersion: String by project
+    implementation(platform("io.micronaut:micronaut-bom:$micronautVersion"))
+    implementation("io.micronaut:micronaut-runtime")
+    implementation("io.micronaut:micronaut-http-server-netty")
+    implementation("io.micronaut:micronaut-http-client")
+    kapt(platform("io.micronaut:micronaut-bom:$micronautVersion"))
+    kapt("io.micronaut:micronaut-inject-java")
+    kapt("io.micronaut:micronaut-validation")
+    kaptTest(platform("io.micronaut:micronaut-bom:$micronautVersion"))
+    kaptTest("io.micronaut:micronaut-inject-java")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.8")
+    testImplementation(platform("io.micronaut:micronaut-bom:$micronautVersion"))
+    testImplementation("io.micronaut.test:micronaut-test-kotlintest")
+
+    runtimeOnly("ch.qos.logback:logback-classic:1.2.3")
+    testImplementation("io.mockk:mockk:1.9.3")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.2")
+}
+
+application {
+    mainClassName = "com.merrylab.example.Application"
+    applicationDefaultJvmArgs = listOf("-noverify", "-XX:TieredStopAtLevel=1", "-Dcom.sun.management.jmxremote")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
+
+allOpen {
+    annotation("io.micronaut.aop.Around")
+}
+
+tasks {
+
+    withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "1.8"
+            javaParameters = true
+        }
+    }
+}
+
+tasks.withType<ShadowJar> {
+    mergeServiceFiles()
+}
